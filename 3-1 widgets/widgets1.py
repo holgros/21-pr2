65a8f33b11cdced8a4ede0ed76136e593858e772
Elev <holgros@gmail.com>
2021-10-28 08:47:05 +0200

    Intro grafiska gränssnitt med Tkinter

---
+++ 3-1 widgets/widgets1.py
@@ -0,0 +1,8 @@
+from tkinter import *   # importera alla metoder i Pythons inbyggda GUI-ramverk Tkinter
+root = Tk()             # skapa ett rot-objekt
+# här behandlas fönstret som ska visas
+img = PhotoImage(file='bild.gif')
+label = Label(image=img)
+label.pack()
+# visa fönstret och håll programmet vid liv tills man stänger fönstret
+root.mainloop()
